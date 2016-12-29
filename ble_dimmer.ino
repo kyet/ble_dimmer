c1134fda8d115df3617dbc2271125b054d4481e1
kyet <kyet@me.com>
2016-12-29 20:25:10 +0900

    v5

--- ble_dimmer.ino
+++ ble_dimmer.ino
@@ -1,6 +1,4 @@
-/* Ref.
- * http://playground.arduino.cc/Code/ACPhaseControl
- * ----------------------------------------------------------------------------
+/* ----------------------------------------------------------------------------
  * "THE BEER-WARE LICENSE" (Revision 42):
  * <kyet@me.com> wrote this file.  As long as you retain this notice you
  * can do whatever you want with this stuff. If we meet some day, and you think
@@ -8,10 +6,19 @@
  * ----------------------------------------------------------------------------
  */
 
+/* References
+ * http://playground.arduino.cc/Main/ACPhaseControl
+ * http://playground.arduino.cc/Code/ACPhaseControl
+ * http://alfadex.com/2014/02/dimming-230v-ac-with-arduino-2/
+ * http://arduinotehniq.blogspot.kr/2014/10/ac-light-dimmer-with-arduino.html
+ * https://arduinodiy.wordpress.com/2012/10/19/dimmer-arduino/
+ * http://www.instructables.com/id/Arduino-controlled-light-dimmer-The-circuit/
+ */
+
 #include <stdarg.h>
-#include <TimerOne.h>
-#include <SoftwareSerial.h>
-#include <PacketSerial.h>
+#include <TimerOne.h>        // https://github.com/PaulStoffregen/TimerOne
+#include <SoftwareSerial.h>  // https://www.arduino.cc/en/Reference/SoftwareSerial
+#include <PacketSerial.h>    // https://github.com/bakercp/PacketSerial
 
 /* device type */
 #define SWITCH       0x01
@@ -19,46 +26,46 @@
 const byte devType = DIMMER;
 
 // undefine for release
-//#define __DEBUG__
+#define __DEBUG__
 
 /* data type */
-#define TYPE_RAW     0x01
-#define TYPE_DIMMING 0x02
-#define TYPE_UNDEF6  0x04
-#define TYPE_UNDEF5  0x08
-#define TYPE_UNDEF4  0x10
-#define TYPE_UNDEF3  0x20
-#define TYPE_UNDEF2  0x40
-#define TYPE_UNDEF1  0x80
+#define TYPE_RAW         0x01
+#define TYPE_DIMMING1    0x02
+#define TYPE_UNDEF6      0x04
+#define TYPE_UNDEF5      0x08
+#define TYPE_UNDEF4      0x10
+#define TYPE_UNDEF3      0x20
+#define TYPE_UNDEF2      0x40
+#define TYPE_UNDEF1      0x80
 
 /* for dimmer */
-#define ZERO_CROSS_INT   0   // for zero crossing detect (int0 is pin 2)
-#define HZ               60  // 60 for the most America, Korea, Taiwan, Phillippines
-                             // 50 for China, Europe, Japan, and rest of.
-#define FUNC_LINEAR  0x01
+#define ZERO_CROSS_INT   0     // for zero crossing detect (int0 is pin 2)
+#define HZ               60    // 60 in the most America, Korea, Taiwan, Phillippines
+                               // 50 in China, Europe, Japan, and rest of.
 
 /* for bluetooth */
-#define BLE_RX        10     // serial pin: connect reverse order
-#define BLE_TX        11
-#define BLE_DATA_MAX  20     // payload size
+#define BLE_RX           10    // serial pin: connect reverse order
+#define BLE_TX           11
+#define BLE_DATA_MAX     20    // payload size
 
 /* outlet pin */
 const byte outlet[] = {3, 5};
 const byte nOutlet  = sizeof(outlet) / sizeof(outlet[0]);
 
 /* for dimmer */
-/* NOTE: calcuation of dimPeriod.
+/* NOTE: calcuation of dimPeriod
  *       sec to us, hertz to frequency, half cycle, range
  *       zero crossing period: 8,333us for 60Hz. 10,000us for 50Hz.
  *       dimPeriod when dimRange is 128: 65 for 60Hz, 78 for 50Hz. 
  *       dimPeriod when dimRange is 258: 32 for 60Hz, 39 for 50Hz. 
- * NOTE: When dim range is 128, below 10 and avobe 124 not works in experimental. 
+ * NOTE: When dim range is 128, 
+         dimming values below 10 and above 124 not works in experimental. 
  *       So we use threshold low and high.
  */
-const byte dimRange  = 128;                 // greater than 128 may not work
-const byte thresLow  = dimRange >> 3;       // approximately 10% of dimRange
-const byte thresHigh = dimRange - thresLow; // approximately 90% of dimRange
-const byte dimPeriod = (byte)((1000.0 * 1000.0) / (HZ * 2 * dimRange));
+const byte dimRange   = 128;                   // greater than 128 may not work
+const byte dimThresLo = dimRange >> 3;         // approximately 10% of dimRange
+const byte dimThresHi = dimRange - dimThresLo; // approximately 90% of dimRange
+const byte dimPeriod  = (byte)((1000.0 * 1000.0) / (HZ * 2 * dimRange));
 volatile byte dimming[nOutlet]  = {0}; // the time to idle low on the outlet[]
 volatile byte crossing[nOutlet] = {0}; // zero-crossing event
 volatile byte trigCnt = 0;
@@ -98,14 +105,15 @@ void zeroCrossInt()
 	// set event flag
 	for (int i=0; i<nOutlet; i++)
 	{
-		if (dimming[i] < thresLow)
-		{
-			digitalWrite(outlet[i], LOW);
-			crossing[i] = 0;
-		}
-		else if (dimming[i] > thresHigh)
+		// dimming[] indicate delay time. lower is on, higher is off
+		if (dimming[i] < dimThresLo)
 		{
 			digitalWrite(outlet[i], HIGH);
+			crossing[i] = 0;
+		}
+		else if (dimming[i] > dimThresHi)
+		{
+			digitalWrite(outlet[i], LOW);
 			crossing[i] = 0;
 		}
 		else
@@ -196,19 +204,15 @@ void bleRaw(byte *data, byte sz)
 }
 
 // y = ax^2 + bx + c
-inline byte bleDimmingLinear(byte x, 
-		int8_t a, byte da, 
-		int8_t b, byte db, 
-		int8_t c, byte dc)
+inline byte bleDimmingLinear(byte x, int8_t a, byte da, int8_t b, byte db, int8_t c)
 {
 	int y = 0;
 	
 	// prevent divide by zero
 	if (da == 0) { da = 1; }
 	if (db == 0) { db = 1; }
-	if (dc == 0) { dc = 1; }
 
-	y = a*x*x/da + b*x/db + c/dc;
+	y = a*x*x/da + b*x/db + c;
 
 	if (y > dimRange) { return dimRange; }
 	else if (y < 0)   { return 0; }
@@ -216,30 +220,32 @@ inline byte bleDimmingLinear(byte x,
 	return y;
 }
 
+/* NOTE: small divider could be OK.
+         you can use 4bit per divider and merge two byte divider into one byte.
+		 then, we can control three outlet within 20 bytes
+ */
 typedef struct {
 	byte   port;
 	byte   duration; // duration * 10 = 1 seconds
-	byte   function;
 	int8_t  a;       // coefficient. signed byte
-	uint8_t da;       // divider for coefficient
+	uint8_t da;      // divider for coefficient
 	int8_t  b;
 	uint8_t db;
-	int8_t  c;
-	uint8_t dc;
+	int8_t  c;       // last constant doesn't need divider
 } dimValue;
 
 /* Payload (data) 
  * .-----------------------------------------.
  * | dimValue1 | dimValue2 | ... | dimValueN |
  * |-----------+-----------+-----+-----------|
- * |         9 |         9 | ... |         9 |
+ * |         7 |         7 | ... |         7 |
  * '-----------------------------------------'
  * N is up to 2 ( (BLE_DATA_MAX-2) / sizeof(dimValue) )
  * duration is transition time
  * if function is y = -1/128x^2 + 2x, you can set
  * (ca, da) = (-1, 128)
  * (cb, db) = ( 2,   1)
- * (cc, dc) = ( 0,   1)
+ * (cc    ) = ( 0     )
  *
  * NOTE: this funtion use busy loop
  */
@@ -266,21 +272,14 @@ void bleDimming(byte *data, byte sz)
 		if (dv->port == 0)           { continue; }
 		dv->port--;
 
-		syslog("LED %d(%dpin) Duration(%d) y=(%d/%d)x^2 + (%d/%d)x + (%d/%d)",
+		syslog("LED %d(%dpin) Duration(%d) y=(%d/%d)x^2 + (%d/%d)x + (%d)",
 				dv->port, outlet[dv->port], dv->duration, 
-				dv->a, dv->da, dv->b, dv->db, dv->c, dv->dc);
+				dv->a, dv->da, dv->b, dv->db, dv->c);
 
 		delta = dv->duration*100 / dimRange;
 		for (int i=0; i<dimRange; i++)
 		{
-			if (dv->function == FUNC_LINEAR)
-				y = bleDimmingLinear(i, 
-						dv->a, dv->da, 
-						dv->b, dv->db,
-						dv->c, dv->dc);
-			else
-				y = 0;
-
+			y = bleDimmingLinear(i, dv->a, dv->da, dv->b, dv->db, dv->c);
 			dimming[dv->port] = dimRange - y;
 			delay(delta);
 		}
@@ -321,7 +320,7 @@ void bleParser(const byte* buffer, size_t size)
 		case TYPE_RAW:
 			bleRaw(datagram + 2, sz - 2);
 			break;
-		case TYPE_DIMMING:
+		case TYPE_DIMMING1:
 			bleDimming(datagram + 2, sz - 2);
 			break;
 		default:
@@ -352,28 +351,26 @@ void debug_dimmer()
 #endif
 
 	// continuous dimming
-#if 1
+#if 0
 	// y = x
-	dimValue dv1 = {1, 50, FUNC_LINEAR, 0, 1, 1, 1, 0, 1};
+	dimValue dv1 = {1, 50, 0, 1, 1, 1, 0};
 	bleDimming((byte*)&dv1, sizeof(dimValue));
 #endif
 #if 0
 	// y = 1/128x^2
-	dimValue dv2 = {1, 50, FUNC_LINEAR, 1, 128, 0, 1, 0, 1};
+	dimValue dv2 = {1, 50, 1, 128, 0, 1, 0};
 	bleDimming((byte*)&dv2, sizeof(dimValue));
 #endif
 #if 0
 	// y = -1/128x^2 + 2x
-	dimValue dv3 = {1, 50, FUNC_LINEAR, -1, 128, 2, 1, 0, 1};
+	dimValue dv3 = {1, 50, -1, 128, 2, 1, 0};
 	bleDimming((byte*)&dv3, sizeof(dimValue));
 #endif
 }
 
 void loop() 
 {
-#if 0
 	pktSerial.update();
-#endif
 	 
 #if defined(__DEBUG__)
 	debug_dimmer();
